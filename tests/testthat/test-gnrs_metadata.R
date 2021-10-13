634df59d2b02b54cdd754af99fed34c1f0509dbb
Brian Maitner <bmaitner@gmail.com>
2021-10-13 19:20:08 +0000

    version 0.3.4

--- tests/testthat/test-gnrs_metadata.R
+++ tests/testthat/test-gnrs_metadata.R
@@ -3,11 +3,12 @@ context("gnrs_metadata")
 
 test_that("example works", {
   
-  skip_if_offline()
-  skip_on_cran()
+  #skip_if_offline()
+  #skip_on_cran()
   
   
-  all_metadata <- GNRS_metadata(url = url)
+  vcr::use_cassette(name = "all_metadata",
+                    {       all_metadata <- GNRS_metadata(url = url) })
   
   expect_equal(object = class(all_metadata), expected = "list")
   
